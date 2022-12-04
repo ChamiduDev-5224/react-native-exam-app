bb74a65894d3b7225908fdcdebee2b3b1ec07db2
Chamidu-circlebook <pplchamiduravihara@gmail.com>
2022-12-04 20:44:05 +0530

    mongo db connection done

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const dotEnv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 7000;
 
+const connectionDb = require("./Config/connection");
 const studentRoute = require("./Routes/student.route");
 const resultsRoute = require("./Routes/results.route");
 app.use(express.json());
+connectionDb();
 app.use("/api/students", studentRoute);
 app.use("/api/results", resultsRoute);
 
