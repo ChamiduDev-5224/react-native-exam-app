cb8cd7eaf98e1a8cbec8e29798fd5bc9941325c6
Chamidu-circlebook <pplchamiduravihara@gmail.com>
2022-12-04 21:03:50 +0530

    files removed
    files removed

--- backend/Routes/results.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getResults, postData } = require("../Controller/results.controller");
-
-router.get("/", getResults);
-router.post("/:id", postData);
-module.exports = router;
--- backend/Routes/student.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllStudent, postData } = require("../Controller/student.controller");
-
-router.get("/", getAllStudent);
-router.post("/:id", postData);
-module.exports = router;
